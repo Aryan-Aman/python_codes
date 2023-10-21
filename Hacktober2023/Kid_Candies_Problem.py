26d60827a9faa2c6e3e8e969336829e19935e39f
Aman <aman209001@gmail.com>
2023-10-21 13:06:33 +0530

    Create Kid_Candies_Problem.py
    Added a new problem

---
+++ Hacktober2023/Kid_Candies_Problem.py
@@ -0,0 +1,11 @@
+'''Leetcode problem 1431 -Kids with greatest number of candies'''
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        m_c=max(candies)
+        for i in range(len(candies)):
+            if candies[i]+extraCandies>=m_c:
+                candies[i]=True
+            else:
+                candies[i]=False
+        return candies
+        
